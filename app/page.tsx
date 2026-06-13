435041c3487dcd66f7c14d2d04500c66e94549ab
cyber-zeff <huzaifashamsi819@gmail.com>
2026-06-13 21:02:13 +0500

    defined the flow of creation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 
 
+// flow: nav -: hero -: footer -: main (form, properties, about, sponsors/partners, contact) -: GSAP animations -: responsive design
+
 export default function Home() {
   return (
     <>
