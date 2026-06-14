cbf2e9aaca14c924815b7a32b05416821723366f
cyber-zeff <huzaifashamsi819@gmail.com>
2026-06-14 13:57:46 +0500

    added navbar to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@
 // flow: nav -: hero -: footer -: main (form, properties, about, sponsors/partners, contact) -: GSAP animations -: responsive design
 // theme: black (111112), white (f1f1f1), and purple (obsidian wala color)
 
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
     <>
-
+      <Navbar />
     </>
   );
 }
