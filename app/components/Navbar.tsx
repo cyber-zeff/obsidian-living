e44a68212b9fefdaaa59c6149d45657bd139ac3e
cyber-zeff <huzaifashamsi819@gmail.com>
2026-06-14 15:45:39 +0500

    started dev for nav

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,73 @@
 
 export default function Navbar() {
-  return (
-    <nav>
-        navbar
-    </nav>
-  )
+    return (
+        <nav className="py-5 flex items-center justify-around priColor">
+            <div className="hFont">Obsidian Living</div>
+            <div>
+                <ul className="flex gap-5">
+                    <li>
+                        <a
+                            href="/"
+                            className="group relative overflow-hidden px-2 py-1 inline-flex items-center justify-center before:absolute before:inset-0
+                            before:bg-[#8b5ce9] before:origin-left before:scale-x-0 before:transition-transform before:duration-300 before:content-[''] hover:before:scale-x-100">
+                            <span className="relative z-10 h-[1.5em] overflow-hidden">
+                                {/* Current text */}
+                                <span
+                                    className="block transition-transform duration-300 group-hover:translate-y-full">
+                                    Home
+                                </span>
+
+                                {/* Incoming text */}
+                                <span
+                                    className="absolute left-0 top-0 block -translate-y-full transition-transform duration-300 group-hover:translate-y-0" >
+                                    Home
+                                </span>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="/"
+                            className="group relative overflow-hidden px-2 py-1 inline-flex items-center justify-center before:absolute before:inset-0
+                            before:bg-[#8b5ce9] before:origin-left before:scale-x-0 before:transition-transform before:duration-300 before:content-[''] hover:before:scale-x-100">
+                            <span className="relative z-10 h-[1.5em] overflow-hidden">
+                                {/* Current text */}
+                                <span
+                                    className="block transition-transform duration-300 group-hover:translate-y-full">
+                                    Buy
+                                </span>
+
+                                {/* Incoming text */}
+                                <span
+                                    className="absolute left-0 top-0 block -translate-y-full transition-transform duration-300 group-hover:translate-y-0" >
+                                    Buy
+                                </span>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="/"
+                            className="group relative overflow-hidden px-2 py-1 inline-flex items-center justify-center before:absolute before:inset-0
+                            before:bg-[#8b5ce9] before:origin-left before:scale-x-0 before:transition-transform before:duration-300 before:content-[''] hover:before:scale-x-100">
+                            <span className="relative z-10 h-[1.5em] overflow-hidden">
+                                {/* Current text */}
+                                <span
+                                    className="block transition-transform duration-300 group-hover:translate-y-full">
+                                    About
+                                </span>
+
+                                {/* Incoming text */}
+                                <span
+                                    className="absolute left-0 top-0 block -translate-y-full transition-transform duration-300 group-hover:translate-y-0" >
+                                    About
+                                </span>
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div>Contact</div>
+        </nav>
+    )
 }
