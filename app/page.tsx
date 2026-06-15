e9d18c1778c0c6a4466f4810370cee174928d7d3
cyber-zeff <huzaifashamsi819@gmail.com>
2026-06-15 11:24:45 +0500

    hero added

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,9 @@ import Hero from "./components/Hero";
 // flow: nav -: hero -: footer -: main (form, properties, about, sponsors/partners, contact) -: GSAP animations -: responsive design
 // theme: black (111112), white (f1f1f1), and purple (obsidian wala color)
 
-import Navbar from "./components/Navbar";
-
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
     </>
   );
