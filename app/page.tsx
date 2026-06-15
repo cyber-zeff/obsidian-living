b9128005789d3512f8d07f2fbc202723ab93cfdd
cyber-zeff <huzaifashamsi819@gmail.com>
2026-06-15 19:59:28 +0500

    add: contact page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 // flow: nav -: hero -: footer -: main (form, properties, about, sponsors/partners, contact) -: GSAP animations -: responsive design
@@ -11,6 +12,7 @@ export default function Home() {
     <>
       <Hero />
       <About />
+      <Contact />
       <Footer />
     </>
   );
