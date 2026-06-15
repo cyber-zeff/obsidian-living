7f6d89229be9bc65233ef8245f1e78d0812fdf9c
cyber-zeff <huzaifashamsi819@gmail.com>
2026-06-15 20:33:06 +0500

    corrected buy link + added sticky

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 const navLinks = [
     { name: "Home", href: "/" },
-    { name: "Buy", href: "/" },
+    { name: "Buy", href: "/Buy" },
     { name: "About", href: "/" },
 ];
 
@@ -28,6 +28,8 @@ export default function Navbar() {
                 ease: [0.22, 1, 0.36, 1],
             }}
             className="
+                sticky
+                bg-[#090909]
                 absolute
                 top-0
                 left-0
