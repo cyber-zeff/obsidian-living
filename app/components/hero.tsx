a33d372273371a9618cd3ae2b1449b44235db934
cyber-zeff <huzaifashamsi819@gmail.com>
2026-06-15 11:25:49 +0500

    upd: fixed flexbox alignment + started working on text + completed framer based animations

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,13 +1,60 @@
+import { motion } from "framer-motion";
 import Image from "next/image";
+import Navbar from "./Navbar";
 
 export default function Hero() {
-  return (
-    <>
-        <div className="priColor">
-        </div>
-        <div>
-            <Image width={600} height={600} src="/obsidian.png" alt="Obsidian Image" />
-        </div>
-    </>
-  )
+    return (
+        <section className="h-screen">
+            <Navbar />
+            <section className="flex justify-center">
+                <div className="min-w-50 priColor self-start mt-[10vh]">
+                    <h2 className="text-3xl">Obsidian</h2>
+                    <motion.div
+                        animate={{
+                            width: ["0%", "100%"],
+                        }}
+                        transition={{
+                            duration: 1,
+                            ease: "easeInOut",
+                        }}
+                        className="relative left-0 top-0 h-[0.65px] bg-[#f1f1f1]"
+                    >
+                    </motion.div>
+                </div>
+                <motion.div
+                    animate={{
+                        y: [5, -5, 5], // up and down float
+                    }}
+                    transition={{
+                        duration: 4,
+                        ease: "easeInOut",
+                        repeat: Infinity,
+                    }}
+                    className="w-fit"
+                >
+                    <Image
+                        width={600}
+                        height={600}
+                        src="/obsidian.png"
+                        alt="Obsidian Image"
+                        priority
+                    />
+                </motion.div>
+                <div className="min-w-50 priColor self-end mb-[35vh]">
+                    <h2 className="text-3xl">Living</h2>
+                    <motion.div
+                        animate={{
+                            width: ["0%", "100%"],
+                        }}
+                        transition={{
+                            duration: 1,
+                            ease: "easeInOut",
+                        }}
+                        className="relative left-0 top-0 h-[0.65px] bg-[#f1f1f1]"
+                    >
+                    </motion.div>
+                </div>
+            </section>
+        </section>
+    )
 }
