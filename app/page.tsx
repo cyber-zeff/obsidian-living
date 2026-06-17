5eed7dc79c8b5ae56252c6a28e39a66f07b424c4
cyber-zeff <huzaifashamsi819@gmail.com>
2026-06-17 15:11:10 +0500

    rem: extra comments

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,6 @@ import PropertiesPreview from "./components/Buy";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-// flow: nav -: hero -: footer -: main (form, properties, about, sponsors/partners, contact) -: GSAP animations -: responsive design
-// theme: black (111112), white (f1f1f1), and purple (obsidian wala color)
 
 export default function Home() {
   return (
